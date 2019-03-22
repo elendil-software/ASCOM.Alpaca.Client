5c1ed3919af5f9af224c6fc24a60a860cd0fa2f5
Julien Tschäppät <git@elendil.software>
2019-03-22 22:06:36 +0100

    Removed commented code

--- src/ASCOM.Alpaca.Client/ICommandSender.cs
+++ src/ASCOM.Alpaca.Client/ICommandSender.cs
@@ -5,10 +5,6 @@ namespace ASCOM.Alpaca.Client
 {
     public interface ICommandSender
     {
-        //bool IsInitialized { get; }
-
-        //void InitRestClient(string baseUrl);
-
         IRestResponse ExecuteRequest(RestRequest request);
 
         TASCOMRemoteResponse ExecuteRequest<TASCOMRemoteResponse>(RestRequest request) where TASCOMRemoteResponse : IResponse, new();
