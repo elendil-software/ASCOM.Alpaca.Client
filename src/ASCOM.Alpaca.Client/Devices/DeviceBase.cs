31880674cceb39d14d94738c1159dd03cce2a390
Julien Tschäppät <git@elendil.software>
2019-03-27 21:12:50 +0100

    Implementation of common methods

---
+++ src/ASCOM.Alpaca.Client/Devices/DeviceBase.cs
@@ -0,0 +1,171 @@
+using System;
+using System.Collections.Generic;
+using ASCOM.Alpaca.Client.Logger;
+using ASCOM.Alpaca.Client.Methods;
+using ASCOM.Alpaca.Client.Request;
+using ASCOM.Alpaca.Client.Responses;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+
+namespace ASCOM.Alpaca.Client.Device
+{
+    public abstract class DeviceBase : ICommonMethods
+    {
+        private readonly ILogger _logger;
+        private readonly ICommandSender _commandSender;
+        private readonly RequestBuilder _requestBuilder;
+        private readonly IClientTransactionIdGenerator _clientTransactionIdGenerator;
+        
+        protected DeviceBase(DeviceType deviceType, int deviceNumber, int clientId, ICommandSender commandSender, IClientTransactionIdGenerator clientTransactionIdGenerator)
+        {
+            _commandSender = commandSender ?? throw new ArgumentNullException(nameof(commandSender));
+            _requestBuilder = new RequestBuilder(deviceType, deviceNumber, clientId);
+        }
+
+        public StringResponse Action(string actionName, string actionParameters)
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                {"Action", actionName},
+                {"Parameters", actionParameters}
+            };
+
+            RestRequest request = _requestBuilder.BuildRestRequest(CommonMethod.Action, Method.PUT, parameters, _clientTransactionIdGenerator.GetTransactionId());
+            _logger.LogInformation(request);
+            
+            var response = _commandSender.ExecuteRequest<StringResponse>(request);
+            _logger.LogInformation(response);
+            
+            return response;
+        }
+
+        public MethodResponse CommandBlind(string command, bool raw = false)
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                {"Command", command},
+                {"Raw", raw.ToString()}
+            };
+            RestRequest request = _requestBuilder.BuildRestRequest(CommonMethod.CommandBlind, Method.PUT, parameters, _clientTransactionIdGenerator.GetTransactionId());
+            _logger.LogInformation(request);
+            
+            var response = _commandSender.ExecuteRequest<MethodResponse>(request);
+            _logger.LogInformation(response);
+
+            return response;
+        }
+
+        public BoolResponse CommandBool(string command, bool raw = false)
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                {"Command", command},
+                {"Raw", raw.ToString()}
+            };
+            RestRequest request = _requestBuilder.BuildRestRequest(CommonMethod.CommandBool, Method.PUT, parameters, _clientTransactionIdGenerator.GetTransactionId());
+            _logger.LogInformation(request);
+            
+            var response = _commandSender.ExecuteRequest<BoolResponse>(request);
+            _logger.LogInformation(response);
+
+            return response;
+        }
+
+        public StringResponse CommandString(string command, bool raw = false)
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                {"Command", command},
+                {"Raw", raw.ToString()}
+            };
+            RestRequest request = _requestBuilder.BuildRestRequest(CommonMethod.CommandString, Method.PUT, parameters, _clientTransactionIdGenerator.GetTransactionId());
+            _logger.LogInformation(request);
+            
+            var response = _commandSender.ExecuteRequest<StringResponse>(request);
+            _logger.LogInformation(response);
+
+            return response;
+        }
+
+        public BoolResponse IsConnected()
+        {
+            RestRequest request = _requestBuilder.BuildRestRequest(CommonMethod.Connected, Method.GET, _clientTransactionIdGenerator.GetTransactionId());
+            _logger.LogInformation(request);
+            
+            var response = _commandSender.ExecuteRequest<BoolResponse>(request);
+            _logger.LogInformation(response);
+
+            return response;
+        }
+
+        public MethodResponse SetConnected(bool connected)
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                {"Connected ", connected.ToString()}
+            };
+            RestRequest request = _requestBuilder.BuildRestRequest(CommonMethod.Connected, Method.PUT, parameters, _clientTransactionIdGenerator.GetTransactionId());
+            _logger.LogInformation(request);
+            
+            var response = _commandSender.ExecuteRequest<MethodResponse>(request);
+            _logger.LogInformation(response);
+
+            return response;
+        }
+
+        public StringResponse GetDescription()
+        {
+            RestRequest request = _requestBuilder.BuildRestRequest(CommonMethod.Description, Method.GET, _clientTransactionIdGenerator.GetTransactionId());
+            _logger.LogInformation(request);
+            
+            var response = _commandSender.ExecuteRequest<StringResponse>(request);
+            _logger.LogInformation(response);
+
+            return response;
+        }
+
+        public StringResponse GetDriverInfo()
+        {
+            RestRequest request = _requestBuilder.BuildRestRequest(CommonMethod.Driverinfo, Method.GET, _clientTransactionIdGenerator.GetTransactionId());
+            _logger.LogInformation(request);
+            
+            var response = _commandSender.ExecuteRequest<StringResponse>(request);
+            _logger.LogInformation(response);
+
+            return response;
+        }
+
+        public StringResponse GetDriverVersion()
+        {
+            RestRequest request = _requestBuilder.BuildRestRequest(CommonMethod.DriverVersion, Method.GET, _clientTransactionIdGenerator.GetTransactionId());
+            _logger.LogInformation(request);
+            
+            var response = _commandSender.ExecuteRequest<StringResponse>(request);
+            _logger.LogInformation(response);
+
+            return response;
+        }
+
+        public StringResponse GetName()
+        {
+            RestRequest request = _requestBuilder.BuildRestRequest(CommonMethod.Name, Method.GET, _clientTransactionIdGenerator.GetTransactionId());
+            _logger.LogInformation(request);
+            
+             var response = _commandSender.ExecuteRequest<StringResponse>(request);
+             _logger.LogInformation(response);
+
+             return response;
+        }
+
+        public StringArrayResponse GetSupportedActions()
+        {
+            RestRequest request = _requestBuilder.BuildRestRequest(CommonMethod.SupportedActions, Method.GET, _clientTransactionIdGenerator.GetTransactionId());
+            _logger.LogInformation(request);
+            
+            var response = _commandSender.ExecuteRequest<StringArrayResponse>(request);
+            _logger.LogInformation(response);
+
+            return response;
+        }
+    }
+}
--- src/ASCOM.Alpaca.Client/Devices/ICommonMethods.cs
+++ src/ASCOM.Alpaca.Client/Devices/ICommonMethods.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using ASCOM.Alpaca.Client.Responses;
 
 namespace ASCOM.Alpaca.Client.Device
 {
@@ -10,14 +11,14 @@ namespace ASCOM.Alpaca.Client.Device
         /// <param name="actionName">A well known name that represents the action to be carried out.</param>
         /// <param name="actionParameters">List of required parameters or an Empty String if none are required</param>
         /// <returns>String response from the device</returns>
-        string Action(string actionName, string actionParameters);
+        StringResponse Action(string actionName, string actionParameters);
         
         /// <summary>
         /// Transmits an arbitrary string to the device and does not wait for a response. Optionally, protocol framing characters may be added to the string before transmission.
         /// </summary>
         /// <param name="command">The literal command string to be transmitted</param>
         /// <param name="raw">If set to true the string is transmitted 'as-is', if set to false then protocol framing characters may be added prior to transmission</param>
-        void CommandBlind(string command, bool raw = false);
+        MethodResponse CommandBlind(string command, bool raw = false);
         
         /// <summary>
         /// Transmits an arbitrary string to the device and waits for a boolean response. Optionally, protocol framing characters may be added to the string before transmission.
@@ -25,7 +26,7 @@ namespace ASCOM.Alpaca.Client.Device
         /// <param name="command">The literal command string to be transmitted</param>
         /// <param name="raw">If set to true the string is transmitted 'as-is', if set to false then protocol framing characters may be added prior to transmission</param>
         /// <returns>Boolean response from the device.</returns>
-        bool CommandBool(string command, bool raw = false);
+        BoolResponse CommandBool(string command, bool raw = false);
         
         /// <summary>
         /// Transmits an arbitrary string to the device and waits for a string response. Optionally, protocol framing characters may be added to the string before transmission.
@@ -33,48 +34,48 @@ namespace ASCOM.Alpaca.Client.Device
         /// <param name="command">The literal command string to be transmitted.</param>
         /// <param name="raw">If set to true the string is transmitted 'as-is', if set to false then protocol framing characters may be added prior to transmission</param>
         /// <returns>String response from the device.</returns>
-        string CommandString(string command, bool raw = false);
+        StringResponse CommandString(string command, bool raw = false);
         
         /// <summary>
         /// Retrieves the connected state of the device
         /// </summary>
         /// <returns><c>True</c> if the device is connected, <c>false</c> if not</returns>
-        bool IsConnected();
+        BoolResponse IsConnected();
         
         /// <summary>
         /// Sets the connected state of the device
         /// </summary>
         /// <param name="connected">Set True to connect to the device hardware, set False to disconnect from the device hardware</param>
-        void SetConnected(bool connected);
+        MethodResponse SetConnected(bool connected);
         
         /// <summary>
         /// Retrives the description of the device
         /// </summary>
         /// <returns></returns>
-        string GetDescription();
+        StringResponse GetDescription();
         
         /// <summary>
         /// Retrives the description of the driver
         /// </summary>
         /// <returns></returns>
-        string GetDriverInfo();
+        StringResponse GetDriverInfo();
         
         /// <summary>
         /// Retrives the version of the driver
         /// </summary>
         /// <returns>A string containing only the major and minor version of the driver.</returns>
-        string GetDriverVersion();
+        StringResponse GetDriverVersion();
         
         /// <summary>
         /// Retrives the name of the device
         /// </summary>
         /// <returns></returns>
-        string GetName();
+        StringResponse GetName();
         
         /// <summary>
         /// Returns the list of action names supported by this driver. 
         /// </summary>
         /// <returns></returns>
-        List<string> GetSupportedActions();
+        StringArrayResponse GetSupportedActions();
     }
 }
