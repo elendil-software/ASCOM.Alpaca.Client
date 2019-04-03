337e020883bba7e0b9bb37987d3317524a33dd5e
Julien Tschäppät <git@elendil.software>
2019-04-03 07:22:51 +0200

    Added numbers for devices types

--- src/ASCOM.Alpaca.Client/Devices/DeviceType.cs
+++ src/ASCOM.Alpaca.Client/Devices/DeviceType.cs
@@ -2,14 +2,14 @@ namespace ASCOM.Alpaca.Client.Devices
 {
     public enum DeviceType
     {
-        Switch,
-        SafetyMonitor,
-        Dome,
-        Camera,
-        ObservingConditions,
-        FilterWheel,
-        Focuser,
-        Rotator,
-        Telescope
+        Switch = 0,
+        SafetyMonitor = 1,
+        Dome = 2,
+        Camera = 3,
+        ObservingConditions = 4,
+        FilterWheel = 5,
+        Focuser = 6,
+        Rotator = 7,
+        Telescope = 8
     }
 }
