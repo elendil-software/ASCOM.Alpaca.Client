8443b9631d6654a10bfce8910afd490a11dc9825
Julien Tschäppät <git@elendil.software>
2019-04-03 07:21:49 +0200

    DeviceType and ClientId added to DeviceConfiguration

--- src/ASCOM.Alpaca.Client/Configuration/DeviceConfiguration.cs
+++ src/ASCOM.Alpaca.Client/Configuration/DeviceConfiguration.cs
@@ -1,3 +1,5 @@
+using ASCOM.Alpaca.Client.Devices;
+
 namespace ASCOM.Alpaca.Client.Configuration
 {
     public class DeviceConfiguration
@@ -6,7 +8,10 @@ namespace ASCOM.Alpaca.Client.Configuration
         public string Host { get; set; } = "127.0.0.1";
         public string ApiVersion { get; set; } = "v1";
         public int Port { get; set; } = 11111;
+
+        public DeviceType DeviceType { get; set; } = DeviceType.Telescope;
         public int DeviceNumber { get; set; } = 0;
+        public int ClientId { get; set; } = 1;
 
         public string GetBaseUrl() => $"{Protocol}{Host}:{Port}/api/{ApiVersion}/";
     }
