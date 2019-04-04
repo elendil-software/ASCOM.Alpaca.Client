32555ab6827dfbcaf4c3ffd8ef747680262777b5
Julien Tschäppät <git@elendil.software>
2019-04-04 18:31:50 +0200

    Device provider has a logger

--- src/ASCOM.Alpaca.Client/Devices/Providers/DeviceProvider.cs
+++ src/ASCOM.Alpaca.Client/Devices/Providers/DeviceProvider.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using ASCOM.Alpaca.Client.Configuration;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace ASCOM.Alpaca.Client.Devices.Providers
@@ -9,10 +10,17 @@ namespace ASCOM.Alpaca.Client.Devices.Providers
     public class DeviceProvider : IDeviceProvider
     {
         private readonly IEnumerable<IDeviceBase> _devices;
+        private readonly ILogger<DeviceProvider> _logger;
         
         public DeviceProvider(IEnumerable<IDeviceBase> devices)
         {
             _devices = devices ?? throw new ArgumentNullException(nameof(devices));
+        }
+        
+        public DeviceProvider(IEnumerable<IDeviceBase> devices, ILogger<DeviceProvider> logger)
+        {
+            _devices = devices ?? throw new ArgumentNullException(nameof(devices));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public T GetDevice<T>(int deviceNumber) where T : IDeviceBase
