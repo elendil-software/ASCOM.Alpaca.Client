2f4155058dda77c3d65c59393f4914c8fec8bbfc
Julien Tschäppät <git@elendil.software>
2019-04-09 18:09:52 +0200

    ISafetyMonitor methods

--- src/ASCOM.Alpaca.Client/Devices/ISafetyMonitor.cs
+++ src/ASCOM.Alpaca.Client/Devices/ISafetyMonitor.cs
@@ -1,7 +1,19 @@
+using System.Threading.Tasks;
+
 namespace ASCOM.Alpaca.Client.Devices
 {
-    public interface ISafetyMonitor
+    public interface ISafetyMonitor : IDevice
     {
+        /// <summary>
+        /// Indicates whether the monitored state is safe for use.
+        /// </summary>
+        /// <returns>True if the state is safe, False if it is unsafe.</returns>
+        bool IsSafe();
         
+        /// <summary>
+        /// Indicates whether the monitored state is safe for use.
+        /// </summary>
+        /// <returns>True if the state is safe, False if it is unsafe.</returns>
+        Task<bool> IsSafeAsync();
     }
 }
