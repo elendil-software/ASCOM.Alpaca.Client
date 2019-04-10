6c7622e9939cd7ed3f3375a75a388a563691b9e6
Julien Tschäppät <git@elendil.software>
2019-04-10 07:28:05 +0200

    IDome methods

---
+++ src/ASCOM.Alpaca.Client/Devices/ShutterState.cs
@@ -0,0 +1,11 @@
+namespace ASCOM.Alpaca.Client.Devices
+{
+    public enum ShutterState
+    {
+        Open = 0,
+        Closed = 1,
+        Opening = 2,
+        Closing = 3,
+        Error = 4
+    }
+}
