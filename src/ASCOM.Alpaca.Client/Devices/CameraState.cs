9e9bcfa74e2a75522107bc5664d101e1699d16d5
Julien Tschäppät <git@elendil.software>
2019-04-12 09:10:01 +0200

    ICamera methods

---
+++ src/ASCOM.Alpaca.Client/Devices/CameraState.cs
@@ -0,0 +1,12 @@
+namespace ASCOM.Alpaca.Client.Devices
+{
+    public enum CameraState
+    {
+        Idle = 0, 
+        Waiting = 1, 
+        Exposing = 2,
+        Reading = 3, 
+        Download = 4, 
+        Error = 5
+    }
+}
