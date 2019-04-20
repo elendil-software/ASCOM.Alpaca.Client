b4e28bb17e1c434bb986a75e12525b98a0f4ee6a
Julien Tschäppät <git@elendil.software>
2019-04-20 14:05:58 +0200

    Demo refactoring, extraction of common parts

---
+++ src/ASCOM.Alpaca.Client.Demo.Desktop/ViewModels/DeviceViewModelBase.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using ASCOM.Alpaca.Client.Devices.Providers;
+using Caliburn.Micro;
+
+namespace ASCOM.Alpaca.Client.Demo.Desktop.ViewModels
+{
+    public class DeviceViewModelBase : Screen
+    {
+        private string _host = "127.0.0.1";
+        private int _port = 11111;
+        private int _clientId = 1;
+        private int _deviceId;
+
+        private string _name = "";
+        private string _description = "";
+        private string _driverVersion = "";
+        private string _driverInfo = "";
+
+        protected readonly IDeviceFactory DeviceFactory;
+
+        public DeviceViewModelBase(IDeviceFactory deviceFactory)
+        {
+            DeviceFactory = deviceFactory ?? throw new ArgumentNullException(nameof(deviceFactory));
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                NotifyOfPropertyChange(() => Name);
+
+            }
+        }
+
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                _description = value;
+                NotifyOfPropertyChange(() => Description);
+
+            }
+        }
+
+        public string DriverVersion
+        {
+            get => _driverVersion;
+            set
+            {
+                _driverVersion = value;
+                NotifyOfPropertyChange(() => DriverVersion);
+
+            }
+        }
+
+        public string DriverInfo
+        {
+            get => _driverInfo;
+            set
+            {
+                _driverInfo = value;
+                NotifyOfPropertyChange(() => DriverInfo);
+
+            }
+        }
+
+        public string Host
+        {
+            get => _host;
+            set
+            {
+                _host = value;
+                NotifyOfPropertyChange(() => Host);
+                NotifyOfPropertyChange(() => CanConnect);
+            }
+        }
+
+        public int Port
+        {
+            get => _port;
+            set
+            {
+                _port = value;
+                NotifyOfPropertyChange(() => Port);
+                NotifyOfPropertyChange(() => CanConnect);
+            }
+        }
+
+        public int ClientId
+        {
+            get => _clientId;
+            set
+            {
+                _clientId = value;
+                NotifyOfPropertyChange(() => ClientId);
+                NotifyOfPropertyChange(() => CanConnect);
+            }
+        }
+
+        public int DeviceId
+        {
+            get => _deviceId;
+            set
+            {
+                _deviceId = value;
+                NotifyOfPropertyChange(() => DeviceId);
+                NotifyOfPropertyChange(() => CanConnect);
+            }
+        }
+
+        public bool CanConnect => !string.IsNullOrEmpty(Host) && Port > 0 && DeviceId >= 0 && ClientId > 0;
+    }
+}
--- src/ASCOM.Alpaca.Client.Demo.Desktop/ViewModels/FilterWheelViewModel.cs
+++ src/ASCOM.Alpaca.Client.Demo.Desktop/ViewModels/FilterWheelViewModel.cs
@@ -5,123 +5,21 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using ASCOM.Alpaca.Client.Configuration;
-using ASCOM.Alpaca.Client.Devices;
 using ASCOM.Alpaca.Client.Devices.FilterWheel;
 using ASCOM.Alpaca.Client.Devices.Providers;
 using ASCOM.Alpaca.Enums.Devices;
-using Caliburn.Micro;
 
 namespace ASCOM.Alpaca.Client.Demo.Desktop.ViewModels
 {
-    public class FilterWheelViewModel : Screen
+    public class FilterWheelViewModel : DeviceViewModelBase
     {
-        private string _host = "127.0.0.1";
-        private int _port = 11111;
-        private int _clientId = 1;
-        private int _deviceId;
-
-        private string _name = "";
-        private string _description = "";
-        private string _driverVersion = "";
-        private string _driverInfo = "";
-
         private int _selectedFilter;
         private int _currentPosition;
 
         private IFilterWheel _filterWheel;
-        private readonly IDeviceFactory _deviceFactory;
 
-        public FilterWheelViewModel(IDeviceFactory deviceFactory)
+        public FilterWheelViewModel(IDeviceFactory deviceFactory) : base(deviceFactory)
         {
-            _deviceFactory = deviceFactory ?? throw new ArgumentNullException(nameof(deviceFactory));
-        }
-
-        public string Name
-        {
-            get => _name;
-            set
-            {
-                _name = value;
-                NotifyOfPropertyChange(() => Name);
-
-            }
-        }
-
-        public string Description
-        {
-            get => _description;
-            set
-            {
-                _description = value;
-                NotifyOfPropertyChange(() => Description);
-
-            }
-        }
-        
-        public string DriverVersion
-        {
-            get => _driverVersion;
-            set
-            {
-                _driverVersion = value;
-                NotifyOfPropertyChange(() => DriverVersion);
-
-            }
-        }
-        
-        public string DriverInfo
-        {
-            get => _driverInfo;
-            set
-            {
-                _driverInfo = value;
-                NotifyOfPropertyChange(() => DriverInfo);
-
-            }
-        }
-
-        public string Host
-        {
-            get => _host;
-            set
-            {
-                _host = value;
-                NotifyOfPropertyChange(() => Host);
-                NotifyOfPropertyChange(() => CanConnect);
-            }
-        }
-
-        public int Port
-        {
-            get => _port;
-            set
-            {
-                _port = value;
-                NotifyOfPropertyChange(() => Port);
-                NotifyOfPropertyChange(() => CanConnect);
-            }
-        }
-
-        public int ClientId
-        {
-            get => _clientId;
-            set
-            {
-                _clientId = value;
-                NotifyOfPropertyChange(() => ClientId);
-                NotifyOfPropertyChange(() => CanConnect);
-            }
-        }
-
-        public int DeviceId
-        {
-            get => _deviceId;
-            set
-            {
-                _deviceId = value;
-                NotifyOfPropertyChange(() => DeviceId);
-                NotifyOfPropertyChange(() => CanConnect);
-            }
         }
 
         public ObservableCollection<int> FocusOffsets { get; set; } = new ObservableCollection<int>();
@@ -148,11 +46,9 @@ namespace ASCOM.Alpaca.Client.Demo.Desktop.ViewModels
             }
         }
 
-        public bool CanConnect => !string.IsNullOrEmpty(Host) && Port > 0 && DeviceId >= 0 && ClientId > 0;
-
         public async Task Connect()
         {
-            _filterWheel = _deviceFactory.GetDevice<FilterWheel>(new DeviceConfiguration
+            _filterWheel = DeviceFactory.GetDevice<FilterWheel>(new DeviceConfiguration
             {
                 ClientId = ClientId, DeviceNumber = DeviceId, DeviceType = DeviceType.FilterWheel, Host = Host,
                 Port = Port
