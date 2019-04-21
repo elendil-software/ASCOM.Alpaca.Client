4263b125c5ae900de303a446021d0cf018908f47
Julien Tschäppät <git@elendil.software>
2019-04-21 12:50:39 +0200

    SafetyMonitor implementation

---
+++ src/ASCOM.Alpaca.Client/Devices/SafetyMonitor.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using ASCOM.Alpaca.Client.Configuration;
+using ASCOM.Alpaca.Client.Devices.Methods;
+using ASCOM.Alpaca.Client.Request;
+using ASCOM.Alpaca.Client.Transactions;
+using ASCOM.Alpaca.Enums.Devices;
+using ASCOM.Alpaca.Responses.Boolean;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+
+namespace ASCOM.Alpaca.Client.Devices
+{
+    public class SafetyMonitor : DeviceBase, ISafetyMonitor
+    {
+        public SafetyMonitor(DeviceConfiguration configuration, IClientTransactionIdGenerator clientTransactionIdGenerator, ICommandSender commandSender, ILogger<DeviceBase> logger) : base(configuration, clientTransactionIdGenerator, commandSender, logger)
+        {
+        }
+
+        public SafetyMonitor(DeviceConfiguration configuration, IClientTransactionIdGenerator clientTransactionIdGenerator, ICommandSender commandSender) : base(configuration, clientTransactionIdGenerator, commandSender)
+        {
+        }
+
+        protected override DeviceType DeviceType { get; } = DeviceType.SafetyMonitor;
+        
+        public bool IsSafe() => ExecuteRequest<bool, BoolResponse>(BuildIsSafeRequest);
+        public async Task<bool> IsSafeAsync() => await ExecuteRequestAsync<bool, BoolResponse>(BuildIsSafeRequest);
+        private RestRequest BuildIsSafeRequest() => RequestBuilder.BuildRestRequest(SafetyMonitorMethod.IsSafe, Method.GET, ClientTransactionIdGenerator.GetTransactionId());
+    }
+}
