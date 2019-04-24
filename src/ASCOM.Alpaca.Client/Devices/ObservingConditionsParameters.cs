74b57e1d62e75f53d94f76375ac486dda0f8bfbc
Julien Tschäppät <git@elendil.software>
2019-04-24 22:31:29 +0200

    Made ObservingConditionsParameters class static

--- src/ASCOM.Alpaca.Client/Devices/ObservingConditionsParameters.cs
+++ src/ASCOM.Alpaca.Client/Devices/ObservingConditionsParameters.cs
@@ -1,6 +1,6 @@
 namespace ASCOM.Alpaca.Client.Devices
 {
-    internal class ObservingConditionsParameters
+    internal static class ObservingConditionsParameters
     {
         public const string SensorName = "SensorName";
         public const string AveragePeriod = "AveragePeriod";
