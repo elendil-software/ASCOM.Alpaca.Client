be3275447ab75383f76787ce5cb9497b11dca118
Julien Tschäppät <git@elendil.software>
2019-05-16 20:21:30 +0200

    Documentation

--- src/ASCOM.Alpaca.Client/Transactions/ClientTransactionIdGenerator.cs
+++ src/ASCOM.Alpaca.Client/Transactions/ClientTransactionIdGenerator.cs
@@ -1,10 +1,20 @@
 namespace ASCOM.Alpaca.Client.Transactions
 {
+    /// <summary>
+    /// Generate a client transaction ID.
+    /// This identifier can be useful to associate events in device and client logs
+    /// </summary>
     public class ClientTransactionIdGenerator : IClientTransactionIdGenerator
     {
         private readonly object _lockObject = new object();
         private int _currentTransactionId;
         
+        /// <summary>
+        /// Generate a client transaction ID.
+        /// The client should start this count at 1 and increment by one on each successive transaction.
+        /// This will aid associating entries in device logs with corresponding entries in client side logs.
+        /// </summary>
+        /// <returns>Generated client transaction ID</returns>
         public int GetTransactionId()
         {
             lock (_lockObject)
--- src/ASCOM.Alpaca.Client/Transactions/IClientTransactionIdGenerator.cs
+++ src/ASCOM.Alpaca.Client/Transactions/IClientTransactionIdGenerator.cs
@@ -1,7 +1,17 @@
 namespace ASCOM.Alpaca.Client.Transactions
 {
+    /// <summary>
+    /// Generate a client transaction ID.
+    /// This identifier can be useful to associate events in device and client logs
+    /// </summary>
     public interface IClientTransactionIdGenerator
     {
+        /// <summary>
+        /// Generate a client transaction ID.
+        /// The client should start this count at 1 and increment by one on each successive transaction.
+        /// This will aid associating entries in device logs with corresponding entries in client side logs.
+        /// </summary>
+        /// <returns>Generated client transaction ID</returns>
         int GetTransactionId();
     }
 }
