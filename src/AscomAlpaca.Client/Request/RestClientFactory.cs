91347e9daa12bde2a2ecf71f6a7a3bcd148d42c7
Julien Tschäppät <git@elendil.software>
2019-05-29 07:25:24 +0200

    RestClientFactory is internal

--- src/AscomAlpaca.Client/Request/RestClientFactory.cs
+++ src/AscomAlpaca.Client/Request/RestClientFactory.cs
@@ -2,7 +2,7 @@ using RestSharp;
 
 namespace ES.AscomAlpaca.Client.Request
 {
-    public class RestClientFactory : IRestClientFactory
+    internal class RestClientFactory : IRestClientFactory
     {
         public IRestClient Create(string baseUrl) => new RestClient(baseUrl);
     }
