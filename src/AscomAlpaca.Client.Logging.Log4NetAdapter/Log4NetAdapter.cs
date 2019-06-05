0ce5f557682e77b2e4287680c808331a5bba2fbe
Julien Tschäppät <git@elendil.software>
2019-06-05 07:24:33 +0200

    Adapter for Log4Net

---
+++ src/AscomAlpaca.Client.Logging.Log4NetAdapter/Log4NetAdapter.cs
@@ -0,0 +1,49 @@
+using System;
+using log4net;
+using log4net.Core;
+
+namespace ES.AscomAlpaca.Client.Logging
+{
+    /// <summary>
+    /// <see cref="ILogger"/> adapter for <see cref="ILog"/>
+    /// </summary>
+    public class Log4NetAdapter : ILogger
+    {
+        private readonly ILog _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ILog" /> class.
+        /// </summary>
+        /// <param name="logger">A configured instance of <see cref="ILog"/></param>
+        public Log4NetAdapter(ILog logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+        
+        /// <inheritdoc/>
+        public void Log(LogEvent logEvent)
+        {
+            switch (logEvent.LogLevel)
+            {
+                case LogLevel.Fatal:
+                    _logger.Fatal(logEvent.Message, logEvent.Exception);
+                    break;
+                case LogLevel.Error:
+                    _logger.Error(logEvent.Message, logEvent.Exception);
+                    break;
+                case LogLevel.Warning:
+                    _logger.Warn(logEvent.Message, logEvent.Exception);
+                    break;
+                case LogLevel.Information:
+                    _logger.Info(logEvent.Message, logEvent.Exception);
+                    break;
+                case LogLevel.Debug:
+                    _logger.Debug(logEvent.Message, logEvent.Exception);
+                    break;
+                default:
+                    _logger.Debug(logEvent.Message, logEvent.Exception);
+                    break;
+            }
+        }
+    }
+}
